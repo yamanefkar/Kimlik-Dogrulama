558ff625e803df3b3251ceda7e12c6d70ceb7e00
yamanefkar <38867900+yamanefkar@users.noreply.github.com>
2019-08-08 12:47:34 +0300

    Update index.php

--- Lib/index.php
+++ Lib/index.php
@@ -1,7 +1,7 @@
 <?php 
 include "kayit.php";
 
-$site = "http://tcno.is-great.org/dorumu.php?isim=".$isim."&soyisim=".$soyad."&dogumyili=".$yil."&tcno=".$tcno;
+$site = "http://tcno.is-great.org/dorumu.php?isim=".$ad."&soyisim=".$soyad."&dogumyili=".$yil."&tcno=".$tcno;
 
 echo " <meta http-equiv='refresh' content='1;URL=".$site."'>";  
 
